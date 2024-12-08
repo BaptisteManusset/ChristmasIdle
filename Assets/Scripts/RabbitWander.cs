39ad23450a97f6112c289804ab90536f15282457
Baptiste Manusset <baptiste.manusset@gmail.com>
2024-12-08 18:03:05 +0100

    wip movement

--- Assets/Scripts/RabbitWander.cs
+++ Assets/Scripts/RabbitWander.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using NaughtyAttributes;
 using Unity.Mathematics;
 using UnityEngine;
@@ -6,126 +7,59 @@ using Random = UnityEngine.Random;
 
 public class RabbitWander : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 2f; // Vitesse de déplacement
-    [SerializeField] private float wanderRange = 5f; // Distance maximale à parcourir autour du point de départ
-    [SerializeField] private float idleTime = 2f; // Temps d'attente entre deux déplacements
-    [SerializeField] private LayerMask groundLayer; // Masque des tiles
-    [SerializeField] private float groundCheckDistance = 0.1f; // Distance pour vérifier la présence de sol
+    public int maxDistance = 4;
+    public Vector3Int nextPos;
+    public int direction = 0;
+    public Vector3Int destination;
+    Tilemap tilemap;
 
-    private Vector2 startPosition;
-    private bool isMoving;
-    private Rigidbody2D rb;
+    public Vector3Int end;
 
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        startPosition = transform.position;
-        rb.simulated = true;
-    }
+    public float speed = .1f;
+
 
     [Button]
-    private void FindNextPosition()
+    public void Test()
     {
-        // Tilemap tm = TilemapHandler.Instance.GetCurrentTilemap();
-
         tilemap = GetComponentInParent<Tilemap>();
-        // Vector3Int coor = tm.WorldToCell(transform.position);
-        // TileBase tile = tm.GetTile(coor);
-
-        nextPos = transform.position + Vector3Int.right * Random.Range(-3, 4);
-        // tile = tilemap.GetTileWorld(nextPos);
-        // bottom = tilemap.GetTileWorld(nextPos + Vector3.down);
-
-        // path = PathExist(tilemap, tilemap.WorldToCell(nextPos), tilemap.WorldToCell(transform.position));
-    }
-
-    bool path;
-    Tilemap tilemap;
-    public TileBase bottom;
-    public Vector3 nextPos;
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = bottom == null ? Color.red : Color.green;
-        Gizmos.DrawWireSphere(nextPos, .25f);
-        Gizmos.DrawLine(nextPos, transform.position);
-
-        HavePath(transform.position, nextPos);
-    }
-
-    private void HavePath(Vector3 a_posA, Vector3 a_posB)
-    {
+        Vector3Int randomPos = Vector3Int.right * Random.Range(-maxDistance, maxDistance + 1);
 
 
-        if (a_posA.x >= a_posB.x)
+        Vector3Int origin = tilemap.WorldToCell(transform.position);
+        end = HavePath(
+            origin,
+            origin + randomPos);
+        if (origin != end)
         {
-            (a_posA, a_posB) = (a_posB, a_posA);
-        }
-
-        var posA = tilemap.WorldToCell(a_posA);
-        var posB = tilemap.WorldToCell(a_posB);
-        int distance = posB.x - posA.x;
-
-        Vector3 offset = new(.5f, .5f, 0);
-        for (int i = 0; i < distance; i++)
-        {
-            TileBase path1 = tilemap.GetTile(posA + Vector3Int.right * i);
-            TileBase ground = tilemap.GetTile(posA + Vector3Int.right * i + Vector3Int.down);
-
-            Gizmos.color = path1 ? Color.red : Color.green;
-            Gizmos.DrawSphere(posA + Vector3Int.right * i + offset, .25f);
-            if (path1) continue;
-
-            Gizmos.color = ground ? Color.green : Color.red;
-            Gizmos.DrawSphere(posA + Vector3Int.right * i + Vector3Int.down + offset, .1f);
+            transform.DOMove(tilemap.GetCellCenterWorld(end), speed);
         }
     }
 
-    private void Update()
+    private Vector3Int HavePath(Vector3Int a_start, Vector3Int a_end)
     {
-        if (Random.value > .5f) return;
-        destination = GetRandomPosition();
+        if (a_start == a_end) return a_start;
+        int distance = a_end.x - a_start.x;
+        direction = (int)math.sign(distance);
 
-        if (Vector2.Distance(destination, transform.position) <= .25f)
+
+        Vector3Int previous = a_start;
+        for (int i = 0; i != (a_end.x - a_start.x) + direction; i += direction)
         {
-            destination = GetRandomPosition();
+            Vector3Int point = a_start + Vector3Int.right * i;
+            bool path = tilemap.GetTile(point) == null;
+            bool ground = tilemap.GetTile(point + Vector3Int.down);
+            Debug.DrawRay(tilemap.GetCellCenterWorld(point), Vector3.up, ground ? Color.green : Color.red, .2f);
+            Debug.DrawRay(tilemap.GetCellCenterWorld(point), Vector3.up, path ? Color.green : Color.red, .2f);
+
+
+            if (!ground || !path)
+            {
+                return previous;
+            }
+
+            previous = point;
         }
 
-        MoveTowards(destination);
-    }
-
-    public Vector3 destination;
-
-
-    private void MoveTowards(Vector2 targetPosition)
-    {
-        Vector2 direction = (targetPosition - (Vector2)transform.position).normalized;
-        rb.velocity = new Vector2(direction.x * moveSpeed, rb.velocity.y);
-
-        // Retourne le sprite selon la direction
-        if (direction.x > 0)
-            transform.localScale = new Vector3(1, 1, 1);
-        else if (direction.x < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
-    }
-
-    private Vector2 GetRandomPosition()
-    {
-        Vector2 randomOffset = new Vector2(Random.Range(-wanderRange, wanderRange), 0);
-        Vector2 targetPosition = startPosition + randomOffset;
-
-        // S'assure que la position est sur le sol
-        if (!IsGroundBelow(targetPosition))
-        {
-            return transform.position;
-        }
-
-        return targetPosition;
-    }
-
-    private bool IsGroundBelow(Vector2 position)
-    {
-        RaycastHit2D hit = Physics2D.Raycast(position, Vector2.down, groundCheckDistance, groundLayer);
-        return hit.collider != null;
+        return a_end;
     }
 }
