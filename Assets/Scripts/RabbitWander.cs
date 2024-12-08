c6820bd4e018236f697de965f963abf1ea2f9a02
Baptiste Manusset <baptiste.manusset@gmail.com>
2024-12-08 23:35:17 +0100

    Working movement todo:  - fix jump  - prevent replacing enviro  - add fall

--- Assets/Scripts/RabbitWander.cs
+++ Assets/Scripts/RabbitWander.cs
@@ -1,38 +1,94 @@
+using System;
 using DG.Tweening;
 using NaughtyAttributes;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.Tilemaps;
 using Random = UnityEngine.Random;
 
 public class RabbitWander : MonoBehaviour
 {
     public int maxDistance = 4;
-    public Vector3Int nextPos;
     public int direction = 0;
-    public Vector3Int destination;
     Tilemap tilemap;
 
     public Vector3Int end;
 
     public float speed = .1f;
 
+    public float duration;
+    public float progress;
+
+    [MinMaxSlider(0, 10)] public Vector2 waitTime;
+
+    public bool moving;
+
+    public Vector3 destination;
+
+    public Vector3 origin;
+
+    private void Awake()
+    {
+        tilemap = GetComponentInParent<Tilemap>();
+    }
+
+    private void OnEnable()
+    {
+        Test();
+    }
+
+    public float distance;
+    public AnimationCurve jumpProfil;
+
+    private void Update()
+    {
+        if (moving)
+        {
+            if (progress >= duration)
+            {
+                progress = 0;
+                moving = false;
+                OnComplete();
+            }
+            else
+            {
+                progress += Time.deltaTime * speed / distance;
+
+                Vector3 newPosition = new(
+                    math.lerp(origin.x, destination.x, progress),
+                    origin.y + jumpProfil.Evaluate(progress));
+                transform.position = newPosition;
+            }
+        }
+    }
 
     [Button]
     public void Test()
     {
-        tilemap = GetComponentInParent<Tilemap>();
         Vector3Int randomPos = Vector3Int.right * Random.Range(-maxDistance, maxDistance + 1);
 
 
-        Vector3Int origin = tilemap.WorldToCell(transform.position);
-        end = HavePath(
-            origin,
-            origin + randomPos);
-        if (origin != end)
+        Vector3Int originCell = tilemap.WorldToCell(transform.position);
+        end = HavePath(originCell, originCell + randomPos);
+        if (originCell != end)
         {
-            transform.DOMove(tilemap.GetCellCenterWorld(end), speed);
+            moving = true;
+            destination = tilemap.GetCellCenterWorld(end);
+            origin = transform.position;
+            distance = Math.Abs(end.x - origin.x);
+            duration = 1;
         }
+        else
+        {
+            OnComplete();
+        }
+    }
+
+    private void OnComplete()
+    {
+        Debug.Log("Complete");
+        Invoke(nameof(Test), Random.Range(waitTime.x, waitTime.y));
     }
 
     private Vector3Int HavePath(Vector3Int a_start, Vector3Int a_end)
